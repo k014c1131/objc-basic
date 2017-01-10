e34587b486edceb1abd056f0b12e074c467882fe
B012 <B012@B012-no-MacBook-Pro.local>
2017-01-10 10:04:09 +0900

    #17 start

---
+++ objc-basic/k014c1131:2.1.2/2.1.2/2.1.2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2.1.2
+//
+//  Created by B012 on 2017/01/10.
+//  Copyright © 2017年 B012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objc-basic/k014c1131:2.1.2/2.1.2/2.1.2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2.1.2
+//
+//  Created by B012 on 2017/01/10.
+//  Copyright © 2017年 B012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
