8d51026e59ab3614661b6038f27c3caa2cfbdce3
ITユーザー <it-user@kpc-it3153mc.kmt.neec.ac.jp>
2017-01-11 21:38:54 +0900

    @k014c1131 #28 start

---
+++ objc-basic/k014c1131:2.1.6/2.1.6/2.1.6/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2.1.6
+//
+//  Created by ITユーザー on 2017/01/11.
+//  Copyright © 2017年 ITユーザー. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <WebKit/WebKit.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
