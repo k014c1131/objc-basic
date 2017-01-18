3843b242e3355a6947cb315003144abe76080737
ITユーザー <it-user@kpc-it3153mc.kmt.neec.ac.jp>
2017-01-18 20:17:51 +0900

    @k014c1131 #29 start

---
+++ objc-basic/k014c1131:2.1.7/2.1.7/2.1.7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2.1.7
+//
+//  Created by ITユーザー on 2017/01/18.
+//  Copyright © 2017年 ITユーザー. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-basic/k014c1131:2.1.7/2.1.7/2.1.7/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2.1.7
+//
+//  Created by ITユーザー on 2017/01/18.
+//  Copyright © 2017年 ITユーザー. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
